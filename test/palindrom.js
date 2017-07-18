eb24c6ca9d5f15031355404000d059cea748159f
jason1213 <boybai1213@gmail.com>
2017-07-18 22:05:01 +0800

    test

--- test/palindrom.js
+++ test/palindrom.js
@@ -1,11 +1,11 @@
-const assert = require("assert");
-const palindrom = require("../palindrom");
+const assert = require('assert');
+const palindrom = require('../palindrom');
 const { checkPalindrom } = palindrom;
 
-describe("Palindrom", () => {
-  describe("#checkPalindrom", () => {
-    it("should be true where str is palindrom", () => {
-      const str = "abccba";
+describe('Palindrom', () => {
+  describe('#checkPalindrom', () => {
+    it('should be true where str is palindrom', () => {
+      const str = 'abccba';
       assert.equal(true, checkPalindrom(str));
     });
   });
