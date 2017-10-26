108429c7219681319f2fa5b512bbf4e3dd12c71c
baiyu <baiyu@admaster.com.cn>
2017-10-26 13:16:27 +0800

    add test case for palindrom

--- test/palindrom.js
+++ test/palindrom.js
@@ -5,16 +5,16 @@ const { checkPalindrom } = palindrom;
 /* eslint no-undef: "off" */
 describe('Palindrom', () => {
   describe('#checkPalindrom', () => {
-    it('should be true where str is palindrom', () => {
+    it('should be true where str is a palindrom', () => {
       const str = 'abcba';
       const out = checkPalindrom(str);
       should(out).be.exactly(true);
     });
 
-    it('should be false where str is not palindrom', () => {
-      const str = 'abaeba';
+    it('should be false where str is not a palindrom', () => {
+      const str = 'abcaba';
       const out = checkPalindrom(str);
-      should(out).be.exactyly(false);
+      should(out).be.exactly(false);
     });
   });
 });
