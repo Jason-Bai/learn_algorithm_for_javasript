14cc611cc2ac17607fae5397fbb69943b5d667c8
Jason-Bai <boybai1213@gmail.com>
2018-06-23 22:31:31 +0800

    add linked-list

---
+++ src/data-structure/linked-list/LinkedList.js
@@ -0,0 +1,191 @@
+import LinkedListNode from './LinkedListNode';
+import Comaprator from '../../utils/comparator/Comparator';
+
+export default class LinkedList {
+  /**
+   * @param {Function} [comparatorFunction]
+   */
+  constructor(comparatorFunction) {
+    /* @var LinkedListNode */
+    this.head = null;
+    /* @var LinkedListNode */
+    this.tail = null;
+
+    this.compare = new Comaprator(comparatorFunction);
+  }
+
+  /**
+   * @param {*} value
+   * @return {LinkedList
+   */
+  prepend(value) {
+    const newHead = new LinkedListNode(value, this.head);
+
+    this.head = newHead;
+
+    if (!this.tail) {
+      this.tail = newHead;
+    }
+
+    return this;
+  }
+
+  /**
+   * @param {*} value
+   * @return LinkedList
+   */
+  append(value) {
+    const newNode = new LinkedListNode(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+
+      return this;
+    }
+
+    this.tail.next = newNode;
+    this.tail = newNode;
+
+    return this;
+  }
+
+  /**
+   * @param {*} value
+   * @return {LinkedListNode}
+   */
+  delete(value) {
+    if (!this.head) {
+      return null;
+    }
+
+    let deletedNode = null;
+
+    // 当linkedList是1->1->1->1，删除1
+    while (this.head && this.compare.equal(this.head.value, value)) {
+      deletedNode = this.head;
+      this.head = this.head.next;
+    }
+
+    // 当linkedList是1->2->3，删除2，currentNode为tail
+    let currentNode = this.head;
+
+    if (currentNode !== null) {
+      while (currentNode.next) {
+        if (this.compare.equal(currentNode.next.value, value)) {
+          deletedNode = currentNode.next;
+          currentNode.next = currentNode.next.next;
+        } else {
+          currentNode = currentNode.next;
+        }
+      }
+    }
+
+    // currentNode为最后节点
+    if (this.compare.equal(this.tail.value, value)) {
+      this.tail = currentNode;
+    }
+
+    return deletedNode;
+  }
+
+  /**
+   * @param {Object} findParams
+   * @param {*} findParams.value
+   * @param {function} findParams.callback
+   * @return {LinkedListNode}
+   */
+  find({ value = undefined, callback = undefined }) {
+    if (!this.head) {
+      return null;
+    }
+
+    let currentNode = this.head;
+
+    while (currentNode) {
+      if (callback && callback(currentNode.value)) {
+        return currentNode;
+      }
+
+      if (value !== undefined && this.compare.equal(currentNode.value, value)) {
+        return currentNode;
+      }
+
+      currentNode = currentNode.next;
+    }
+
+    return null;
+  }
+
+  /**
+   * @return {LinkedListNode}
+   */
+  deleteTail() {
+    if (this.head === this.tail) {
+      const deletedTail = this.tail;
+      this.head = null;
+      this.tail = null;
+
+      return deletedTail;
+    }
+
+    const deletedTail = this.tail;
+
+    let currentNode = this.head;
+
+    while (currentNode.next) {
+      if (!currentNode.next.next) {
+        currentNode.next = null;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+
+    this.tail = currentNode;
+
+    return deletedTail;
+  }
+
+  /**
+   * @return {LinkedListNode}
+   */
+  deleteHead() {
+    if (!this.head) {
+      return null;
+    }
+
+    const deletedHead = this.head;
+
+    if (this.head.next) {
+      this.head = this.head.next;
+    } else {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return deletedHead;
+  }
+
+  /**
+   * @return [LinkedListNode[]]
+   */
+  toArray() {
+    const nodes = [];
+
+    let currentNode = this.head;
+
+    while (currentNode) {
+      nodes.push(currentNode);
+      currentNode = currentNode.next;
+    }
+
+    return nodes;
+  }
+
+  /**
+   * @param {function} [callback]
+   * @return {string}
+   */
+  toString(callback) {
+    return this.toArray().map(node => node.toString(callback).toString()).toString();
+  }
+}
