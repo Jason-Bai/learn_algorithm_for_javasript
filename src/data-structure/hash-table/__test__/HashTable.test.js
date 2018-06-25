f668d1582c53a54e7a17ff5e8a911a84e0ec17f5
Jason-Bai <boybai1213@gmail.com>
2018-06-25 23:15:24 +0800

    hashtable test done

--- src/data-structure/hash-table/__test__/HashTable.test.js
+++ src/data-structure/hash-table/__test__/HashTable.test.js
@@ -16,4 +16,75 @@ describe('HashTable', () => {
     expect(hashTable.hash('b')).toBe(2);
     expect(hashTable.hash('abc')).toBe(6);
   });
+
+  it('should set, read and delete data with collisions', () => {
+    const hashTable = new HashTable(3);
+
+    expect(hashTable.hash('a')).toBe(1);
+    expect(hashTable.hash('b')).toBe(2);
+    expect(hashTable.hash('c')).toBe(0);
+    expect(hashTable.hash('d')).toBe(1);
+
+    hashTable.set('a', 'sky-old');
+    hashTable.set('a', 'sky');
+    hashTable.set('b', 'sea');
+    hashTable.set('c', 'earth');
+    hashTable.set('d', 'ocean');
+
+    expect(hashTable.has('x')).toBeFalsy();
+    expect(hashTable.has('b')).toBeTruthy();
+    expect(hashTable.has('c')).toBeTruthy();
+
+    const stringifier = value => `${value.key}:${value.value}`;
+
+    expect(hashTable.buckets[0].toString(stringifier)).toBe('c:earth');
+    expect(hashTable.buckets[1].toString(stringifier)).toBe('a:sky,d:ocean');
+    expect(hashTable.buckets[2].toString(stringifier)).toBe('b:sea');
+
+    expect(hashTable.get('a')).toBe('sky');
+    expect(hashTable.get('d')).toBe('ocean');
+    expect(hashTable.get('x')).not.toBeDefined();
+
+    hashTable.delete('a');
+
+    expect(hashTable.delete('not-existing')).toBeNull();
+
+    expect(hashTable.get('a')).not.toBeDefined();
+    expect(hashTable.get('d')).toBe('ocean');
+
+    hashTable.set('d', 'ocean-new');
+    expect(hashTable.get('d')).toBe('ocean-new');
+  });
+
+  it('should be possible to add objects to hash table', () => {
+    const hashTable = new HashTable();
+
+    hashTable.set('objectKey', { prop1: 'a', prop2: 'b' });
+
+    const object = hashTable.get('objectKey');
+
+    expect(object).toBeDefined();
+    expect(object.prop1).toBe('a');
+    expect(object.prop2).toBe('b');
+  });
+
+  it('should track actual keys', () => {
+    const hashTable = new HashTable();
+
+    hashTable.set('a', 'sky-old');
+    hashTable.set('a', 'sky');
+    hashTable.set('b', 'sea');
+    hashTable.set('c', 'earth');
+    hashTable.set('d', 'ocean');
+
+    expect(hashTable.getKeys()).toEqual(['a', 'b', 'c', 'd']);
+    expect(hashTable.has('a')).toBeTruthy();
+    expect(hashTable.has('x')).toBeFalsy();
+
+    hashTable.delete('a');
+
+    expect(hashTable.has('a')).toBeFalsy();
+    expect(hashTable.has('b')).toBeTruthy();
+    expect(hashTable.has('x')).toBeFalsy();
+  });
 });
